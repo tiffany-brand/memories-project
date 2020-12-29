d797c4e9dad2d4bf98cfd0c0fd2003eecaf705e1
Tiffany Brand <brand2learn@gmail.com>
2020-12-28 23:12:47 -0500

    Connect to DB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extend: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extend: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	})
+	.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+	.catch((error) => console.log(error.message));
