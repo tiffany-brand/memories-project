6e7dc6f4a6d08671d93d0b6452401a963fad6551
Tiffany Brand <brand2learn@gmail.com>
2020-12-29 12:08:12 -0500

    Fix form styling

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -19,7 +19,7 @@ const Form = () => {
 
 	return (
 		<Paper className={classes.paper}>
-			<form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+			<form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
 				<Typography variant="h6">Creating a Memory</Typography>
 				<TextField
 					name="creator"
