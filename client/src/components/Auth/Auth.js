12383bda0bc4cc50ed8a3dd34b67a50c6f143fb6
Tiffany Brand <brand2learn@gmail.com>
2021-02-02 19:51:36 -0500

    Toggle sign in-up

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -9,13 +9,19 @@ const Auth = () => {
     const classes = useStyles();
 
     const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
 
-    const isSignup = false;
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
     const handleSubmit = () => { };
     const handleChange = () => { };
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        setShowPassword(false);
+    };
+
+
 
     return (
         <Container component="main" maxWidth="xs">
@@ -41,6 +47,13 @@ const Auth = () => {
                     <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
                         {isSignup ? 'Sign Up' : 'Sign In'}
                     </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {isSignup ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}
+                            </Button>
+                        </Grid>
+                    </Grid>
                 </form>
             </Paper>
         </Container>
