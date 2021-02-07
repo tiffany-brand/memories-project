1c35c6cbc2d2241891691a4406e5eaa6b2242cb8
Tiffany Brand <brand2learn@gmail.com>
2021-02-07 13:45:18 -0500

    Add auth to create post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,8 +7,7 @@ import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
-	const [ postData, setPostData ] = useState({
-		creator: '',
+	const [postData, setPostData] = useState({
 		title: '',
 		message: '',
 		tags: '',
@@ -17,28 +16,28 @@ const Form = ({ currentId, setCurrentId }) => {
 	const post = useSelector((state) => (currentId ? state.posts.find((p) => p._id === currentId) : null));
 	const classes = useStyles();
 	const dispatch = useDispatch();
+	const user = JSON.parse(localStorage.getItem('profile'));
 
 	useEffect(
 		() => {
 			if (post) setPostData(post);
 		},
-		[ post ]
+		[post]
 	);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
 		if (currentId) {
-			dispatch(updatePost(currentId, postData));
+			dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
 		} else {
-			dispatch(createPost(postData));
+			dispatch(createPost({ ...postData, name: user?.result?.name }));
 		}
 		clear();
 	};
 	const clear = () => {
 		setCurrentId(null);
 		setPostData({
-			creator: '',
 			title: '',
 			message: '',
 			tags: '',
@@ -46,18 +45,20 @@ const Form = ({ currentId, setCurrentId }) => {
 		});
 	};
 
+	if (!user?.result?.name) {
+		return (
+			<Paper className={classes.paper}>
+				<Typography variant="h6" align="center">
+					Please Sign in to create your own memories and like other's memories.
+				</Typography>
+			</Paper>
+		)
+	}
+
 	return (
 		<Paper className={classes.paper}>
 			<form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
 				<Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
-				<TextField
-					name="creator"
-					variant="outlined"
-					label="Creator"
-					fullWidth
-					value={postData.creator}
-					onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
-				/>
 				<TextField
 					name="title"
 					variant="outlined"
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,15 +7,16 @@ const auth = async (req, res, next) => {
     try {
         // const token = req.headers["Authorization"];
         const token = req.headers.authorization.split(" ")[1];
-        console.log(token);
         const isCustomAuth = token && token.length < 500;
 
         let decodedData;
 
         if (token && isCustomAuth) {
+
             decodedData = jwt.verify(token, process.env.JWT_SECRET);
 
             req.userId = decodedData?.id;
+
         } else {
             decodedData = jwt.decode(token);
 
