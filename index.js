55c4c9bef5667cd33ede16ad2e60923e849ef373
Tiffany Brand <brand2learn@gmail.com>
2021-02-07 15:52:13 -0500

    Serve React app

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
+import path from 'path';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+}
+
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
@@ -23,6 +33,8 @@ mongoose
 		useUnifiedTopology: true,
 		useCreateIndex: true,
 		useFindAndModify: false
-	})
-	.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-	.catch((error) => console.log(error.message));
+	});
+
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+
